76b09fdad405bd854d4f73e9987cbab5d37534df
harrylee <22520435@gm.uit.edu.vn>
2024-10-09 12:55:45 +0700

    Modify AlarmActivity

--- app/src/main/java/com/example/clockdraw/AlarmActivity.java
+++ app/src/main/java/com/example/clockdraw/AlarmActivity.java
@@ -2,6 +2,7 @@ package com.example.clockdraw;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -9,7 +10,9 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.ImageButton;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +24,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class AlarmActivity extends AppCompatActivity {
 
@@ -34,16 +39,6 @@ public class AlarmActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        FloatingActionButton addAlarmButton = findViewById(R.id.addButton);
-        addAlarmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle the click event
-                showDialog();
-            }
-        });
-
 
         BottomNavigationView bottomNavView = findViewById(R.id.bottomNavView);
         bottomNavView.setSelectedItemId(R.id.bottom_alarm);
@@ -74,39 +69,62 @@ public class AlarmActivity extends AppCompatActivity {
             }
             else return false;
         });
-    }
 
-    private void showDialog() {
-        // Show dialog to add alarm
-        final Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.bottom_dialog);
+        FloatingActionButton addAlarmButton = (FloatingActionButton) findViewById(R.id.addButton);
+        RecyclerView mRecyclerview = (RecyclerView) findViewById(R.id.alarmList);
+        mRecyclerview.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        ArrayList<Model> dataHolder = new ArrayList<Model>();                                               //Array list to add reminders and display in recyclerview
+        myAdapter adapter;
 
-        TextView cancelButton = dialog.findViewById(R.id.cancelButton);
-        TextView doneButton = dialog.findViewById(R.id.doneButton);
-
-        cancelButton.setOnClickListener(new View.OnClickListener() {
+        addAlarmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dialog.dismiss();
+                // Handle the click event
+                Intent intent = new Intent(getApplicationContext(), ReminderActivity.class);
+                startActivity(intent);
             }
         });
+        Cursor cursor = new dbManager(getApplicationContext()).readallreminders();                  //Cursor To Load data From the database
+        while (cursor.moveToNext()) {
+            Model model = new Model(cursor.getString(1), cursor.getString(2), cursor.getString(3));
+            dataHolder.add(model);
+        }
 
-        doneButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-                Toast toast = Toast.makeText(AlarmActivity.this, "Alarm added", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 650);
-                toast.show();
-
-            }
-        });
-
-        dialog.show();
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
-        dialog.getWindow().setGravity(Gravity.BOTTOM);
+        adapter = new myAdapter(dataHolder);
+        mRecyclerview.setAdapter(adapter);
     }
+
+//    private void showDialog() {
+//        // Show dialog to add alarm
+//        final Dialog dialog = new Dialog(this);
+//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        dialog.setContentView(R.layout.bottom_dialog);
+//
+//        TextView cancelButton = dialog.findViewById(R.id.cancelButton);
+//        TextView doneButton = dialog.findViewById(R.id.doneButton);
+//
+//        cancelButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dialog.dismiss();
+//            }
+//        });
+//
+//        doneButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dialog.dismiss();
+//                Toast toast = Toast.makeText(AlarmActivity.this, "Alarm added", Toast.LENGTH_SHORT);
+//                toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 650);
+//                toast.show();
+//
+//            }
+//        });
+//
+//        dialog.show();
+//        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+//        dialog.getWindow().setGravity(Gravity.BOTTOM);
+//    }
 }
