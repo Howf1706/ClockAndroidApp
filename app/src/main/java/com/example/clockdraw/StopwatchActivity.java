990f82562dddfd859e2488f4766c10b75d6ea6c9
Hieriss <tranminhhieeus@gmail.com>
2024-10-09 09:08:52 +0700

    recommit

--- app/src/main/java/com/example/clockdraw/StopwatchActivity.java
+++ app/src/main/java/com/example/clockdraw/StopwatchActivity.java
@@ -2,6 +2,13 @@ package com.example.clockdraw;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +18,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.text.MessageFormat;
+import java.util.Locale;
+
 public class StopwatchActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button start, stop, reset;
+    int seconds, minutes, milliSeconds;
+    long milliseconds, startTime, timeBuff, updateTime = 0L;
+    Handler handler;
+    private final Runnable updateTimerThread = new Runnable() {
+        @Override
+        public void run() {
+            milliseconds = SystemClock.uptimeMillis() - startTime;
+            updateTime = timeBuff + milliseconds;
+            seconds = (int) (updateTime / 1000);
+            minutes = seconds / 60;
+            seconds = seconds % 60;
+            milliSeconds = (int) (updateTime % 1000);
+
+            textView.setText(MessageFormat.format("{0}:{1}:{2}", minutes, String.format(Locale.getDefault(),"%02d", seconds), String.format(Locale.getDefault(), "%02d", milliSeconds)));
+            handler.postDelayed(this, 0);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +53,56 @@ public class StopwatchActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textView = findViewById(R.id.textView3);
+        reset = findViewById(R.id.reset);
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+
+        handler = new Handler(Looper.getMainLooper());
+
+        start.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                    startTime = SystemClock.uptimeMillis();
+                    handler.postDelayed(updateTimerThread, 0);
+                    reset.setEnabled(false);
+                    start.setEnabled(false);
+                    stop.setEnabled(true);
+             }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timeBuff += milliseconds;
+                handler.removeCallbacks(updateTimerThread);
+                reset.setEnabled(true);
+                start.setEnabled(true);
+                stop.setEnabled(false);
+                start.setText("Continue");
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timeBuff = 0L;
+                startTime = 0L;
+                updateTime = 0L;
+                milliseconds = 0L;
+                seconds = 0;
+                minutes = 0;
+                milliSeconds = 0;
+                textView.setText("00:00:00");
+                start.setText("Start");
+                reset.setEnabled(false);
+                start.setEnabled(true);
+                stop.setEnabled(false);
+            }
+        });
+
+        textView.setText("00:00:00");
 
         BottomNavigationView bottomNavView = findViewById(R.id.bottomNavView);
         bottomNavView.setSelectedItemId(R.id.bottom_stopwatch);
